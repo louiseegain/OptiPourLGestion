42d58b25e8e24959983f191f4603063f1bcf8a22
Louise EGAIN <louiseegain2401@gmail.com>
2022-11-22 21:39:37 +0100

    correction

--- fonction.php
+++ fonction.php
@@ -8,9 +8,9 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 //  - LSA : permet de calculer le ratio LSA pour un nombre de machine et une liste de      //
 //         tâches données en paramètre.                                                    //
-//  - LPT : utilise la fonction LSA précédente pour calculer le ratio LP mais en triant    //
+//  - LPT : utilise la fonction LSA précédente pour calculer le ratio LPT mais en triant    //
 //          la liste des tâches avant de la                                                //
-//          la passé dans la fonction.                                                     //
+//          passé dans la fonction.                                                     //
 //  - RMA : Permet de calculer le ratio RMA pour un nombre de machine et une liste de      //
 //         tâches données en paramètre.                                                    //
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,13 +21,13 @@
 /*******************************************************************************************/
 /*                                                                                         */
 /*  Description : Cette fonction utilise l'alghoritme LSA. Au debut on va créer un tableau */
-/*  de la taille du nombre de machines entrée en paramètre. Ensuite pour chaqu'une des     */
-/*  taches contenu dans la liste entrée en paramètre, nous allons chercher quelle machine  */
-/*  est libre en première pour y ajouter notre taches.                                     */
-/*  Au fur et a mesure nous calculons la moyenne de la durée des taches ainsi que la tache */
-/*  la plus longue ( en durée).                                                            */
-/*  Une fois l'attribution des taches pour chaque machines terminée, nous calculons le     */
-/*  ratio LSA que nous retounons a l'appelleur.                                            */
+/*  de la taille du nombre de machines entrée en paramètre. Ensuite pour chacune des       */
+/*  tâches contenues dans la liste entrée en paramètre, nous allons chercher quelle machine*/
+/*  est libre en première pour y ajouter notre tâche.                                      */
+/*  Au fur et à mesure nous calculons la moyenne de la durée des tâches ainsi que la tâche */
+/*  la plus longue (en durée).                                                             */
+/*  Une fois l'attribution des tâches pour chaque machines terminée, nous calculons le     */
+/*  ratio LSA que nous retournons a l'appelleur.                                           */
 /*                                                                                         */
 /*******************************************************************************************/
 /*                                                                                         */
@@ -45,21 +45,21 @@ function LSA (int $nbr_machine, Array $taches){
     //on termine de définit la taille de tab_machines par rapport au nombre de machines passé en paramètre et on créer un tableau a l'interieur
     for($i=0 ; $i< $nbr_machine ;$i++){$tab_machines[$i]= array();}
 
-    //réalisé pour chacune des taches contenues dans le tableau de tacges passé en paramètre
+    //réalisé pour chacune des taches contenues dans le tableau de tâches passées en paramètre
     for($i=0 ; $i<count($taches) ;$i++){
-        //déclaration des varaibles ayant besoin d'etre réinitialisée a chaque exécution de la boucle 
+        //déclaration des variables ayant besoin d'etre réinitialisée a chaque exécution de la boucle 
         $max = 0;
         $num_machine = 0;
 
-        //addition de la durée de toutes les taches pour le calcul de la moyenne à la fin
+        //addition de la durée de toutes les tâches pour le calcul de la moyenne à la fin
         $moyenne_taches += $taches[$i];
 
-        //modification de la taches la plus grande si la taille de la taches actuelle est plus grande que celle enregistrée 
+        //modification de la tâches la plus grande si la taille de la tâches actuelle est plus grande que celle enregistrée 
         if($taches>$plus_grande_tache){
             $plus_grande_tache = $taches[$i];
         }
         
-        //récupération de la machine qui sera libre en dernière pour ensuite recuperer celle qui sera libre en premiere
+        //récupération de la machine qui sera libre en dernière pour ensuite récupérer celle qui sera libre en premiere
         for($a=0 ; $a < $nbr_machine ;$a++){
             if(count($tab_machines[$a])>$max){
                 $max = count($tab_machines[$a]);
@@ -75,7 +75,7 @@ function LSA (int $nbr_machine, Array $taches){
             }
         }
 
-        //on ajoute n fois un case au tableau ( n représentant la durée de la tache)
+        //on ajoute n fois un case au tableau ( n représentant la durée de la tâche)
         for($taille_tache = 0; $taille_tache < $taches[$i]; $taille_tache++){
             $tab_machines[$num_machine][$taille_tache+$min] = $taches[$i];
         }
@@ -126,7 +126,7 @@ function LSA (int $nbr_machine, Array $taches){
 /*******************************************************************************************/
 /*                                                                                         */
 /*  Description : Pour cette fonction le principe est le même que pour LSA mais            */
-/*  les taches sont attribués aléatoirement à des machines et non a celle qui a terminée   */
+/*  les tâches sont attribuées aléatoirement à des machines et non a celle qui a terminée  */
 /*  en première.                                                                           */
 /*                                                                                         */
 /*******************************************************************************************/
@@ -143,24 +143,24 @@ function RMA($nbr_machine,$taches){
     $moyenne_taches = 0;        //pour calculer le ratio à la fin
     $max = 0;
 
-    //on termine de définit la taille de tab_machines par rapport au nombre de machines passé en paramètre et on créer un tableau a l'interieur
+    //on termine de définir la taille de tab_machines par rapport au nombre de machines passées en paramètre et on cré un tableau a l'intérieur
     for($i=0 ; $i< $nbr_machine ;$i++){$tab_machines[$i]= array();}
         
-    //réalisé pour chacune des taches contenues dans le tableau de tacges passé en paramètre 
+    //réalisé pour chacune des taches contenues dans le tableau de tâches passées en paramètre 
     for($i=0 ; $i<count($taches) ;$i++){
-        //déclaration des varaibles ayant besoin d'etre réinitialisée a chaque exécution de la boucle 
+        //déclaration des variables ayant besoin d'etre réinitialisée a chaque exécution de la boucle 
         $max = 0;
         $num_machine = 0;
         $num_machine = rand(0,($nbr_machine-1));
 
-        //addition de la durée de toutes les taches pour le calcul de la moyenne à la fin
+        //addition de la durée de toutes les tâches pour le calcul de la moyenne à la fin
         $moyenne_taches += $taches[$i];
 
-        //modification de la taches la plus grande si la taille de la taches actuelle est plus grande que celle enregistrée 
+        //modification de la tâches la plus grande si la taille de la tâches actuelle est plus grande que celle enregistrée 
         if($taches>$plus_grande_tache){
             $plus_grande_tache = $taches[$i];
         }
-        //on ajoute n fois un case au tableau ( n représentant la durée de la tache)
+        //on ajoute n fois un case au tableau ( n représentant la durée de la tâche)
         for($taille_tache = 0; $taille_tache < $taches[$i]; $taille_tache++){
             $tab_machines[$num_machine][count($tab_machines[$num_machine])]=$taches[$i];
         }
